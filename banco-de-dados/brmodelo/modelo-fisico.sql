8b8a1dd3beade1822be78d3a14e8a5c6eec81196
Elzilane Barreto <elzilanebarreto@gmail.com>
2025-02-07 15:22:33 -0300

    ajustes no banco

--- banco-de-dados/brmodelo/modelo-fisico.sql
+++ banco-de-dados/brmodelo/modelo-fisico.sql
@@ -1,21 +1,32 @@
--- Geraï¿½ï¿½o de Modelo fï¿½sico
+-- Geração de Modelo físico
 -- Sql ANSI 2003 - brModelo.
 
+
+
+CREATE TABLE Reserva (
+id_reserva INTEGER PRIMARY KEY,
+data_reserva DATETIME,
+id_cliente INTEGER
+)
+
 CREATE TABLE Destino (
+id_destino INTEGER PRIMARY KEY,
 cidade VARCHAR(100),
-descricao VARCHAR(10),
-paï¿½s VARCHAR(100),
-nome_destino VARCHAR(100),
-id_destino INTEGER PRIMARY KEY
-);
+país VARCHAR(100),
+descricao VARCHAR(100)
+)
 
 CREATE TABLE Pacote (
-descricao VARCHAR(10),
 id_pacote INTEGER PRIMARY KEY,
 preco DECIMAL(5,2),
+duracao_dias INTEGER,
+descricao VARCHAR(10),
+quantidade_disponivel INTEGER,
 id_destino INTEGER,
-FOREIGN KEY(id_destino) REFERENCES Destino (id_destino)
-);
+id_reserva INTEGER,
+FOREIGN KEY(id_destino) REFERENCES Destino (id_destino),
+FOREIGN KEY(id_reserva) REFERENCES Reserva (id_reserva)
+)
 
 CREATE TABLE Pagamento (
 metodo VARCHAR(11),
@@ -24,35 +35,20 @@ id_pagamento INTEGER PRIMARY KEY,
 total DECIMAL(5,2),
 stauts VARCHAR(15),
 id_cliente INTEGER,
-id_reserva INTEGER
-);
-
-CREATE TABLE Reserva_Pacote (
-id_reserva INTEGER,
 id_pacote INTEGER,
-quantidade INTEGER,
 FOREIGN KEY(id_pacote) REFERENCES Pacote (id_pacote)
-);
+)
 
 CREATE TABLE Cliente (
-email VARCHAR(150),
-senha VARCHAR(10),
 id_cliente INTEGER PRIMARY KEY,
 nome_cliente VARCHAR(100),
 endereco VARCHAR(150),
 cpf VARCHAR(10),
-data_cadastro DATETIME
-);
+data_cadastro DATETIME,
+senha VARCHAR(10),
+email VARCHAR(150),
+telefone VARCHAR(15)
+)
 
-CREATE TABLE Reserva (
-id_pacote VARCHAR(10),
-data_reserva DATETIME,
-status VARCHAR(15),
-id_reserva INTEGER PRIMARY KEY,
-id_cliente INTEGER,
-FOREIGN KEY(id_cliente) REFERENCES Cliente (id_cliente)
-);
-
-ALTER TABLE Pagamento ADD FOREIGN KEY(id_cliente) REFERENCES Cliente (id_cliente);
-ALTER TABLE Pagamento ADD FOREIGN KEY(id_reserva) REFERENCES Reserva (id_reserva);
-ALTER TABLE Reserva_Pacote ADD FOREIGN KEY(id_reserva) REFERENCES Reserva (id_reserva);
+ALTER TABLE Reserva ADD FOREIGN KEY(id_cliente) REFERENCES Cliente (id_cliente)
+ALTER TABLE Pagamento ADD FOREIGN KEY(id_cliente) REFERENCES Cliente (id_cliente)
